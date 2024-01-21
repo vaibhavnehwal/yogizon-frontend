f45248d3e4d93397f24e9ce4e6c40f4d23a016b9
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-01-21 14:03:56 +0530

    Navbar added, Course Cards added, Pagination added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Routers from "./Component/Routers/Routers";
 
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello, from YogiZon!</h1>
+        <Routers />
       </div>
     </>
   );
---
+++ src/Component/Routers/Routers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+import Products from '../Products/Products'
+
+function Routers() {
+  return (
+    <div>
+        <Navbar />
+        <Products/>
+        <Footer />
+    </div>
+  )
+}
+
+export default Routers
