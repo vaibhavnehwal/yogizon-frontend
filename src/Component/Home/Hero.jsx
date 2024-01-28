d5372f5e135fa3917789f93ae706f575094d89a5
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-01-29 02:51:31 +0530

    Landing Page Structure complete

---
+++ src/Component/Home/Hero.jsx
@@ -0,0 +1,61 @@
+
+
+function Hero() {
+  return (
+    <div>
+      <div className="relative overflow-hidden">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+          <div className="max-w-2xl text-center mx-auto">
+            <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl">
+              Embark on Your Yoga Journey with{" "}
+              <span className="text-[#ffb606]">Expert Guidance</span>
+            </h1>
+            <p className="mt-3 text-lg text-gray-800">
+              Our curated yoga courses offer a harmonious blend of tradition and
+              innovation, tailored just for you
+            </p>
+          </div>
+
+          <div className="mt-10 relative max-w-5xl mx-auto">
+            <div className="w-full object-cover h-96 sm:h-[480px] bg-[url('https://images.unsplash.com/photo-1606868306217-dbf5046868d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1981&q=80')] bg-no-repeat bg-center bg-cover rounded-xl"></div>
+
+            <div className="absolute inset-0 w-full h-full">
+              <div className="flex flex-col justify-center items-center w-full h-full">
+                <a
+                  className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                  href="#"
+                >
+                  <svg
+                    className="flex-shrink-0 w-4 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <polygon points="5 3 19 12 5 21 5 3" />
+                  </svg>
+                  Play the overview
+                </a>
+              </div>
+            </div>
+
+            <div className="absolute bottom-12 -start-20 -z-[1] w-48 h-48 bg-gradient-to-b from-orange-500 to-white p-px rounded-lg">
+              <div className="bg-white w-48 h-48 rounded-lg"></div>
+            </div>
+
+            <div className="absolute -top-12 -end-20 -z-[1] w-48 h-48 bg-gradient-to-t from-blue-600 to-cyan-400 p-px rounded-full">
+              <div className="bg-white w-48 h-48 rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Saptrishi from '../Home/Saptrishi'
+import React from "react";
+import Saptrishi from "../Home/Saptrishi";
+import Hero from "./Hero";
+import Features from "./Features";
+import AboutUs from "./AboutUs";
+import CardLink from "./CardLink";
+import FeaturesShort from "./FeaturesShort";
+import LatestNews from "./LatestNews";
+import Testimonial from "./Testimonial";
+import Gallery from "./Gallery";
+import CookieBanner from "../CookieBanner/CookieBanner";
+import FAQs from "./FAQs";
 
 function Home() {
   return (
     <div>
-        <Saptrishi />
+      <div className="min-h-screen my-[6rem]">
+        <Hero />
+      </div>
+      <div>
+        <FeaturesShort />
+      </div>
+      <div>
+        <Features />
+      </div>
+      <div className="">
+        <LatestNews />
+      </div>
+      <div>
+        <Testimonial/>
+      </div>
+      <div>
+        <FAQs />
+      </div>
+      <div>
+        <AboutUs />
+      </div>
+      <div>
+        <CardLink />
+      </div>
+      <Gallery />
+      <Saptrishi />
+      <CookieBanner />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
